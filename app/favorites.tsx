becdcc4e59f6db014f68dae1ad5e376408a3caa1
Michał Bryk <galeriakoloru90210@gmail.com>
2024-01-13 11:06:13 +0100

    finish app, add animation to favorites

--- app/favorites.tsx
+++ app/favorites.tsx
@@ -1,8 +1,22 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { storage } from '@/api/mmkv';
 import { Pokemon, getPokemonDetail } from '@/api/pokeapi';
 import { useQueries } from '@tanstack/react-query';
+import Animated, {
+  FadeIn,
+  Layout,
+  SlideOutLeft,
+} from 'react-native-reanimated';
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
   const [keys, setKeys] = useState(storage.getAllKeys());
@@ -17,12 +31,42 @@ const Page = () => {
       };
     }),
   });
+
+  const allFinished = pokemonQueries.every((q) => q.isSuccess);
+
+  useEffect(() => {
+    if (allFinished) {
+      const newData = pokemonQueries.map((q) => q.data!);
+      setData(newData);
+    }
+  }, [allFinished]);
+
+  const removeFavorite = (id: number) => {
+    storage.delete(`favorite-${id}`);
+    setData(data.filter((item) => item.id !== id));
+  };
+
   return (
     <ScrollView>
-      {pokemonQueries.map((q) => (
-        <Text>{q.data?.name}</Text>
-      ))}
-      <Text>favorites</Text>
+      {data.length > 0 &&
+        data.map((item, index) => (
+          <Animated.View
+            key={item.id}
+            layout={Layout.delay(100)}
+            style={styles.item}
+            entering={FadeIn.delay(100 * index)}
+            exiting={SlideOutLeft.duration(200)}
+          >
+            <Image
+              source={{ uri: item.sprites.front_default }}
+              style={styles.preview}
+            />
+            <Text style={styles.itemText}>{item.name}</Text>
+            <TouchableOpacity onPress={() => removeFavorite(item.id)}>
+              <Ionicons name="trash" size={18} color="#c10505" />
+            </TouchableOpacity>
+          </Animated.View>
+        ))}
     </ScrollView>
   );
 };
